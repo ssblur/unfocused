4524b462ce9154304a7d0c1384f74bee308be13e
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-01-08 21:04:33 -0500

    Add pools accessor

---
+++ common/src/main/java/com/ssblur/unfocused/mixin/LootTableAccessor.java
@@ -0,0 +1,19 @@
+package com.ssblur.unfocused.mixin;
+
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(LootTable.class)
+public interface LootTableAccessor {
+  @Accessor
+  List<LootPool> getPools();
+
+  @Mutable
+  @Accessor
+  void setPools(List<LootPool> pools);
+}
--- common/src/main/kotlin/com/ssblur/unfocused/event/common/LootTablePopulateEvent.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/event/common/LootTablePopulateEvent.kt
@@ -1,9 +1,17 @@
 package com.ssblur.unfocused.event.common
 
 import com.ssblur.unfocused.event.SimpleEvent
+import com.ssblur.unfocused.mixin.LootTableAccessor
 import net.minecraft.resources.ResourceKey
+import net.minecraft.world.level.storage.loot.LootPool
 import net.minecraft.world.level.storage.loot.LootTable
 
 object LootTablePopulateEvent: SimpleEvent<LootTablePopulateEvent.LootTableContext>(retroactive = true, cancelable = false) {
-    data class LootTableContext(val id: ResourceKey<LootTable>, val isBuiltin: Boolean, val builder: LootTable)
+    data class LootTableContext(val id: ResourceKey<LootTable>, val isBuiltin: Boolean, val pool: LootTable) {
+        var pools: List<LootPool>
+            get() = (pool as LootTableAccessor).pools
+            set(pools) {
+                (pool as LootTableAccessor).pools = pools
+            }
+    }
 }
