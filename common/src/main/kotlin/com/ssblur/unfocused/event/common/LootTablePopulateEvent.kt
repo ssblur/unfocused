91b344e385264a75597194e307c3a65497178199
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-01-08 21:35:18 -0500

    Remove pools prop

--- common/src/main/kotlin/com/ssblur/unfocused/event/common/LootTablePopulateEvent.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/event/common/LootTablePopulateEvent.kt
@@ -1,18 +1,11 @@
 package com.ssblur.unfocused.event.common
 
 import com.ssblur.unfocused.event.SimpleEvent
-import com.ssblur.unfocused.mixin.LootTableAccessor
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.level.storage.loot.LootPool
 import net.minecraft.world.level.storage.loot.LootTable
 
 @Suppress("unused")
 object LootTablePopulateEvent: SimpleEvent<LootTablePopulateEvent.LootTableContext>(retroactive = true, cancelable = false) {
-    data class LootTableContext(val id: ResourceKey<LootTable>, val isBuiltin: Boolean, val pool: MutableList<LootPool.Builder>) {
-        var pools: List<LootPool>
-            get() = (pool as LootTableAccessor).pools
-            set(pools) {
-                (pool as LootTableAccessor).pools = pools
-            }
-    }
+    data class LootTableContext(val id: ResourceKey<LootTable>, val isBuiltin: Boolean, val pool: MutableList<LootPool.Builder>)
 }
