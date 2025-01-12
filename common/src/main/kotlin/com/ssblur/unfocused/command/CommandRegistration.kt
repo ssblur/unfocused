3859a945562b0e12d4838bf65e6612dd7719ecd5
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-01-11 23:03:15 -0500

    Add helper to register commands

--- common/src/main/kotlin/com/ssblur/unfocused/command/CommandRegistration.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/command/CommandRegistration.kt
@@ -1,11 +1,13 @@
 package com.ssblur.unfocused.command
 
 import com.mojang.brigadier.CommandDispatcher
+import com.ssblur.unfocused.ModInitializer
 import com.ssblur.unfocused.event.SimpleEvent
 import net.minecraft.commands.CommandBuildContext
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 
+@Suppress("unusedreceiverparameter", "unused")
 object CommandRegistration: SimpleEvent<CommandRegistration.CommandRegistrationCallback>(retroactive = true) {
     fun interface CommandRegistrationCallback {
         fun callback(
@@ -13,5 +15,9 @@ object CommandRegistration: SimpleEvent<CommandRegistration.CommandRegistrationC
             registry: CommandBuildContext,
             selection: Commands.CommandSelection
         )
+    }
+
+    fun ModInitializer.registerCommand(callback: CommandRegistrationCallback) {
+        CommandRegistration.callback(callback)
     }
 }
