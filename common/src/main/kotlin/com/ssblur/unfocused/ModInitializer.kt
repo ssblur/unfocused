ea4222292b8c7ac275636872de4e25bdc945ee8e
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-02-02 14:44:19 -0500

    Add Menu registry to ModInitializer

--- common/src/main/kotlin/com/ssblur/unfocused/ModInitializer.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/ModInitializer.kt
@@ -17,6 +17,8 @@ import net.minecraft.tags.TagKey
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
+import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.inventory.MenuType
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.crafting.Recipe
@@ -34,100 +36,130 @@ import java.util.function.Supplier
 
 @Suppress("unused", "unchecked_cast")
 open class ModInitializer(val id: String) {
-    val CONFIG: Config by lazy { Config(id) }
-    val config: GameRuleConfig by lazy { GameRuleConfig(this) }
+  val CONFIG: Config by lazy { Config(id) }
+  val config: GameRuleConfig by lazy { GameRuleConfig(this) }
 
-    var BLOCKS = RegistryTypes.BLOCK.create(id)
-    var ITEMS = RegistryTypes.ITEM.create(id)
-    var EFFECTS = RegistryTypes.EFFECTS.create(id)
-    val BLOCK_ENTITIES = RegistryTypes.BLOCK_ENTITIES.create(id)
-    val DATA_COMPONENTS = RegistryTypes.DATA_COMPONENTS.create(id)
-    val ENTITIES = RegistryTypes.ENTITIES.create(id)
-    val FEATURES = RegistryTypes.FEATURES.create(id)
-    val RECIPES = RegistryTypes.RECIPES.create(id)
-    val RECIPE_TYPES = RegistryTypes.RECIPE_TYPES.create(id)
-    val LOOT_FUNCTION_TYPES = RegistryTypes.LOOT_FUNCTION_TYPES.create(id)
-    val LOOT_CONDITION_TYPES = RegistryTypes.LOOT_CONDITION_TYPES.create(id)
-    val TRIGGER_TYPES = RegistryTypes.TRIGGER_TYPES.create(id)
-    val RECIPE_SERIALIZERS = RegistryTypes.RECIPE_SERIALIZERS.create(id)
-    val CREATIVE_TABS = RegistryTypes.CREATIVE_TABS.create(id)
-    val PARTICLE_TYPES = RegistryTypes.PARTICLE_TYPES.create(id)
+  var BLOCKS = RegistryTypes.BLOCK.create(id)
+  var ITEMS = RegistryTypes.ITEM.create(id)
+  var EFFECTS = RegistryTypes.EFFECTS.create(id)
+  val BLOCK_ENTITIES = RegistryTypes.BLOCK_ENTITIES.create(id)
+  val DATA_COMPONENTS = RegistryTypes.DATA_COMPONENTS.create(id)
+  val ENTITIES = RegistryTypes.ENTITIES.create(id)
+  val FEATURES = RegistryTypes.FEATURES.create(id)
+  val RECIPES = RegistryTypes.RECIPES.create(id)
+  val RECIPE_TYPES = RegistryTypes.RECIPE_TYPES.create(id)
+  val LOOT_FUNCTION_TYPES = RegistryTypes.LOOT_FUNCTION_TYPES.create(id)
+  val LOOT_CONDITION_TYPES = RegistryTypes.LOOT_CONDITION_TYPES.create(id)
+  val TRIGGER_TYPES = RegistryTypes.TRIGGER_TYPES.create(id)
+  val RECIPE_SERIALIZERS = RegistryTypes.RECIPE_SERIALIZERS.create(id)
+  val CREATIVE_TABS = RegistryTypes.CREATIVE_TABS.create(id)
+  val PARTICLE_TYPES = RegistryTypes.PARTICLE_TYPES.create(id)
+  val MENUS = RegistryTypes.MENUS.create(id)
 
-    fun registerBlock(id: String, supplier: Supplier<Block>): RegistrySupplier<Block> {
-        return BLOCKS.register(id, supplier)
-    }
+  fun registerBlock(id: String, supplier: Supplier<Block>): RegistrySupplier<Block> {
+    return BLOCKS.register(id, supplier)
+  }
 
-    fun registerBlockWithItem(id: String, supplier: Supplier<Block>): Pair<RegistrySupplier<Block>, RegistrySupplier<Item>> {
-        val block = BLOCKS.register(id, supplier)
-        val item = ITEMS.register(id) { BlockItem(block.get(), Item.Properties()) }
-        return Pair(block, item)
-    }
+  fun registerBlockWithItem(
+    id: String,
+    supplier: Supplier<Block>
+  ): Pair<RegistrySupplier<Block>, RegistrySupplier<Item>> {
+    val block = BLOCKS.register(id, supplier)
+    val item = ITEMS.register(id) { BlockItem(block.get(), Item.Properties()) }
+    return Pair(block, item)
+  }
 
-    fun registerItem(id: String, supplier: Supplier<Item>): RegistrySupplier<Item> {
-        return ITEMS.register(id, supplier)
-    }
+  fun registerItem(id: String, supplier: Supplier<Item>): RegistrySupplier<Item> {
+    return ITEMS.register(id, supplier)
+  }
 
-    fun registerEffect(id: String, supplier: Supplier<MobEffect>): RegistrySupplier<MobEffect> {
-        return EFFECTS.register(id, supplier)
-    }
+  fun registerEffect(id: String, supplier: Supplier<MobEffect>): RegistrySupplier<MobEffect> {
+    return EFFECTS.register(id, supplier)
+  }
 
-    fun <T: BlockEntity> registerBlockEntity(id: String, supplier: Supplier<BlockEntityType<T>>): RegistrySupplier<BlockEntityType<T>> {
-        return BLOCK_ENTITIES.register(id, supplier as Supplier<BlockEntityType<*>>) as RegistrySupplier<BlockEntityType<T>>
-    }
+  fun <T: BlockEntity> registerBlockEntity(
+    id: String,
+    supplier: Supplier<BlockEntityType<T>>
+  ): RegistrySupplier<BlockEntityType<T>> {
+    return BLOCK_ENTITIES.register(id, supplier as Supplier<BlockEntityType<*>>) as RegistrySupplier<BlockEntityType<T>>
+  }
 
-    fun <T> registerDataComponent(id: String, consumer: Consumer<DataComponentType.Builder<T>>): DataComponentType<T>{
-        val builder = DataComponentType.builder<T>()
-        consumer.accept(builder)
-        val componentType = builder.build()
-        DATA_COMPONENTS.register(id) { componentType }
-        return componentType
-    }
+  fun <T> registerDataComponent(id: String, consumer: Consumer<DataComponentType.Builder<T>>): DataComponentType<T> {
+    val builder = DataComponentType.builder<T>()
+    consumer.accept(builder)
+    val componentType = builder.build()
+    DATA_COMPONENTS.register(id) { componentType }
+    return componentType
+  }
 
-    fun <T: Entity> registerEntity(id: String, supplier: Supplier<EntityType<T?>>): RegistrySupplier<EntityType<T>> {
-        return ENTITIES.register(id, supplier as Supplier<EntityType<*>>) as RegistrySupplier<EntityType<T>>
-    }
+  fun <T: Entity> registerEntity(id: String, supplier: Supplier<EntityType<T?>>): RegistrySupplier<EntityType<T>> {
+    return ENTITIES.register(id, supplier as Supplier<EntityType<*>>) as RegistrySupplier<EntityType<T>>
+  }
 
-    fun registerFeature(id: String, supplier: Supplier<Feature<*>>): RegistrySupplier<Feature<*>> {
-        return FEATURES.register(id, supplier)
-    }
+  fun registerFeature(id: String, supplier: Supplier<Feature<*>>): RegistrySupplier<Feature<*>> {
+    return FEATURES.register(id, supplier)
+  }
 
-    fun registerRecipe(id: String, supplier: Supplier<Recipe<*>>): RegistrySupplier<Recipe<*>> {
-        return RECIPES.register(id, supplier)
-    }
+  fun registerRecipe(id: String, supplier: Supplier<Recipe<*>>): RegistrySupplier<Recipe<*>> {
+    return RECIPES.register(id, supplier)
+  }
 
-    fun registerRecipeType(id: String, supplier: Supplier<RecipeType<*>>): RegistrySupplier<RecipeType<*>> {
-        return RECIPE_TYPES.register(id, supplier)
-    }
+  fun registerRecipeType(id: String, supplier: Supplier<RecipeType<*>>): RegistrySupplier<RecipeType<*>> {
+    return RECIPE_TYPES.register(id, supplier)
+  }
 
-    fun <T: LootItemFunction> registerLootFunction(id: String, supplier: Supplier<LootItemFunctionType<T>>): RegistrySupplier<LootItemFunctionType<T>> {
-        return LOOT_FUNCTION_TYPES.register(id, supplier as Supplier<LootItemFunctionType<*>>) as RegistrySupplier<LootItemFunctionType<T>>
-    }
+  fun <T: LootItemFunction> registerLootFunction(
+    id: String,
+    supplier: Supplier<LootItemFunctionType<T>>
+  ): RegistrySupplier<LootItemFunctionType<T>> {
+    return LOOT_FUNCTION_TYPES.register(
+      id,
+      supplier as Supplier<LootItemFunctionType<*>>
+    ) as RegistrySupplier<LootItemFunctionType<T>>
+  }
 
-    fun registerLootCondition(id: String, supplier: Supplier<LootItemConditionType>): RegistrySupplier<LootItemConditionType> {
-        return LOOT_CONDITION_TYPES.register(id, supplier)
-    }
+  fun registerLootCondition(
+    id: String,
+    supplier: Supplier<LootItemConditionType>
+  ): RegistrySupplier<LootItemConditionType> {
+    return LOOT_CONDITION_TYPES.register(id, supplier)
+  }
 
-    fun <T : CriterionTrigger<*>> registerTrigger(id: String, supplier: Supplier<T>): RegistrySupplier<T> {
-        return TRIGGER_TYPES.register(id, supplier as Supplier<CriterionTrigger<*>>) as RegistrySupplier<T>
-    }
-    fun registerGenericTrigger(id: String): RegistrySupplier<GenericTrigger> {
-        return TRIGGER_TYPES.register(id) { GenericTrigger(location(id)) } as RegistrySupplier<GenericTrigger>
-    }
+  fun <T: CriterionTrigger<*>> registerTrigger(id: String, supplier: Supplier<T>): RegistrySupplier<T> {
+    return TRIGGER_TYPES.register(id, supplier as Supplier<CriterionTrigger<*>>) as RegistrySupplier<T>
+  }
 
-    fun <T: Recipe<*>> registerRecipeSerializer(id: String, supplier: Supplier<RecipeSerializer<T>>): RegistrySupplier<RecipeSerializer<T>> {
-        return RECIPE_SERIALIZERS.register(id, supplier as Supplier<RecipeSerializer<*>>) as RegistrySupplier<RecipeSerializer<T>>
-    }
+  fun registerGenericTrigger(id: String): RegistrySupplier<GenericTrigger> {
+    return TRIGGER_TYPES.register(id) { GenericTrigger(location(id)) } as RegistrySupplier<GenericTrigger>
+  }
 
-    fun <T: ParticleOptions> registerParticleType(id: String, supplier: Supplier<ParticleType<T>>): RegistrySupplier<ParticleType<T>> {
-        return PARTICLE_TYPES.register(id, supplier as Supplier<ParticleType<*>>) as RegistrySupplier<ParticleType<T>>
-    }
+  fun <T: Recipe<*>> registerRecipeSerializer(
+    id: String,
+    supplier: Supplier<RecipeSerializer<T>>
+  ): RegistrySupplier<RecipeSerializer<T>> {
+    return RECIPE_SERIALIZERS.register(
+      id,
+      supplier as Supplier<RecipeSerializer<*>>
+    ) as RegistrySupplier<RecipeSerializer<T>>
+  }
 
-    fun location(path: String) = ResourceLocation.fromNamespaceAndPath(id, path)
+  fun <T: ParticleOptions> registerParticleType(
+    id: String,
+    supplier: Supplier<ParticleType<T>>
+  ): RegistrySupplier<ParticleType<T>> {
+    return PARTICLE_TYPES.register(id, supplier as Supplier<ParticleType<*>>) as RegistrySupplier<ParticleType<T>>
+  }
 
-    fun <T> registerTag(registry: ResourceKey<Registry<T>>, path: ResourceLocation) = TagKey.create(registry, path)
-    fun <T> registerTag(registry: ResourceKey<Registry<T>>, path: String) = registerTag(registry, location(path))
-    fun registerItemTag(path: ResourceLocation) = registerTag(Registries.ITEM, path)
-    fun registerItemTag(path: String) = registerItemTag(location(path))
-    fun registerBlockTag(path: ResourceLocation) = registerTag(Registries.BLOCK, path)
-    fun registerBlockTag(path: String) = registerBlockTag(location(path))
+  fun <T: AbstractContainerMenu> registerMenu(id: String, supplier: Supplier<MenuType<T>>): Supplier<MenuType<T>> {
+    return MENUS.register(id, supplier as Supplier<MenuType<*>>) as Supplier<MenuType<T>>
+  }
+
+  fun location(path: String) = ResourceLocation.fromNamespaceAndPath(id, path)
+
+  fun <T> registerTag(registry: ResourceKey<Registry<T>>, path: ResourceLocation) = TagKey.create(registry, path)
+  fun <T> registerTag(registry: ResourceKey<Registry<T>>, path: String) = registerTag(registry, location(path))
+  fun registerItemTag(path: ResourceLocation) = registerTag(Registries.ITEM, path)
+  fun registerItemTag(path: String) = registerItemTag(location(path))
+  fun registerBlockTag(path: ResourceLocation) = registerTag(Registries.BLOCK, path)
+  fun registerBlockTag(path: String) = registerBlockTag(location(path))
 }
--- common/src/main/kotlin/com/ssblur/unfocused/registry/RegistryTypes.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/registry/RegistryTypes.kt
@@ -5,36 +5,37 @@ import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
 
 object RegistryTypes {
-    open class RegistryType<T>(val key: ResourceKey<net.minecraft.core.Registry<T>>) {
-        private val registries: ArrayList<Registry<T>> = arrayListOf()
-        private val subscribers: ArrayList<Subscriber<T>> = arrayListOf()
+  open class RegistryType<T>(val key: ResourceKey<net.minecraft.core.Registry<T>>) {
+    private val registries: ArrayList<Registry<T>> = arrayListOf()
+    private val subscribers: ArrayList<Subscriber<T>> = arrayListOf()
 
-        fun create(id: String): Registry<T> {
-            val registry: Registry<T> = Registry(id, key)
-            registries += registry
-            subscribers.forEach { registry.subscribe(it) }
-            return registry
-        }
-
-        fun subscribe(subscriber: Subscriber<T>) {
-            registries.forEach { it.subscribe(subscriber) }
-            subscribers += subscriber
-        }
+    fun create(id: String): Registry<T> {
+      val registry: Registry<T> = Registry(id, key)
+      registries += registry
+      subscribers.forEach { registry.subscribe(it) }
+      return registry
     }
 
-    val ITEM = RegistryType(Registries.ITEM)
-    val BLOCK = RegistryType(Registries.BLOCK)
-    val EFFECTS = RegistryType(Registries.MOB_EFFECT)
-    val BLOCK_ENTITIES = RegistryType(Registries.BLOCK_ENTITY_TYPE)
-    val DATA_COMPONENTS = RegistryType(Registries.DATA_COMPONENT_TYPE)
-    val ENTITIES = RegistryType(Registries.ENTITY_TYPE)
-    val FEATURES = RegistryType(Registries.FEATURE)
-    val RECIPE_TYPES = RegistryType(Registries.RECIPE_TYPE)
-    val RECIPES = RegistryType(Registries.RECIPE)
-    val RECIPE_SERIALIZERS = RegistryType(Registries.RECIPE_SERIALIZER)
-    val LOOT_FUNCTION_TYPES = RegistryType(Registries.LOOT_FUNCTION_TYPE)
-    val LOOT_CONDITION_TYPES = RegistryType(Registries.LOOT_CONDITION_TYPE)
-    val TRIGGER_TYPES = RegistryType(Registries.TRIGGER_TYPE)
-    val CREATIVE_TABS = RegistryType(Registries.CREATIVE_MODE_TAB)
-    val PARTICLE_TYPES = RegistryType(Registries.PARTICLE_TYPE)
+    fun subscribe(subscriber: Subscriber<T>) {
+      registries.forEach { it.subscribe(subscriber) }
+      subscribers += subscriber
+    }
+  }
+
+  val ITEM = RegistryType(Registries.ITEM)
+  val BLOCK = RegistryType(Registries.BLOCK)
+  val EFFECTS = RegistryType(Registries.MOB_EFFECT)
+  val BLOCK_ENTITIES = RegistryType(Registries.BLOCK_ENTITY_TYPE)
+  val DATA_COMPONENTS = RegistryType(Registries.DATA_COMPONENT_TYPE)
+  val ENTITIES = RegistryType(Registries.ENTITY_TYPE)
+  val FEATURES = RegistryType(Registries.FEATURE)
+  val RECIPE_TYPES = RegistryType(Registries.RECIPE_TYPE)
+  val RECIPES = RegistryType(Registries.RECIPE)
+  val RECIPE_SERIALIZERS = RegistryType(Registries.RECIPE_SERIALIZER)
+  val LOOT_FUNCTION_TYPES = RegistryType(Registries.LOOT_FUNCTION_TYPE)
+  val LOOT_CONDITION_TYPES = RegistryType(Registries.LOOT_CONDITION_TYPE)
+  val TRIGGER_TYPES = RegistryType(Registries.TRIGGER_TYPE)
+  val CREATIVE_TABS = RegistryType(Registries.CREATIVE_MODE_TAB)
+  val PARTICLE_TYPES = RegistryType(Registries.PARTICLE_TYPE)
+  val MENUS = RegistryType(Registries.MENU)
 }
