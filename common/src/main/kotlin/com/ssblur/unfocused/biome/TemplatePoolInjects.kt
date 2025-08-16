39cc159b130839bcb09a958d6f3a08b4861cb477
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-08-15 23:14:18 -0400

    Fix disconnect signature Update gitignore Make TemplatePoolInjects fail easy until a remedy is found

--- common/src/main/kotlin/com/ssblur/unfocused/biome/TemplatePoolInjects.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/biome/TemplatePoolInjects.kt
@@ -1,9 +1,15 @@
 package com.ssblur.unfocused.biome
 
+import com.mojang.datafixers.util.Pair
 import com.ssblur.unfocused.Unfocused
 import com.ssblur.unfocused.data.DataLoaderRegistry.registerSimpleDataLoader
+import com.ssblur.unfocused.mixin.StructureTemplatePoolAccessor
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.MinecraftServer
+import net.minecraft.server.RegistryLayer
+import net.minecraft.world.level.levelgen.structure.pools.SinglePoolElement
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool
 
 object TemplatePoolInjects {
@@ -26,27 +32,38 @@ object TemplatePoolInjects {
   val injects = Unfocused.registerSimpleDataLoader("unfocused/template_pool_injects", PoolInject::class)
 
   fun inject(server: MinecraftServer) {
-    server.registryAccess().listRegistries().forEach { println(it) }
-//    val poolRegistry = server.registryAccess().get(Registries.TEMPLATE_POOL).get()
-//    val processorLists = server.registryAccess().get(Registries.PROCESSOR_LIST).get()
-//
-//    injects.forEach { (resourceLocation, poolInject) ->
-//      val pool = poolRegistry.value().get(poolInject.pool)!! as StructureTemplatePoolAccessor
-//      val templates = pool.templates
-//      val rawTemplates = pool.rawTemplates
-//
-//      poolInject.elements.forEach { element ->
-//        val poolElement = SinglePoolElement.legacy(
-//            element.element.location,
-//            processorLists.value().get(ResourceKey.create(
-//              Registries.PROCESSOR_LIST,
-//              element.element.processors ?: ResourceLocation.parse("minecraft:empty")
-//            )).orElseThrow()
-//          ).apply(element.element.projection)
-//        for(i in 0..(element.weight ?: 2))
-//          templates.add(poolElement)
-//        pool.rawTemplates = rawTemplates + Pair(poolElement, element.weight)
-//      }
-//    }
+    val worldgen = server.registries().getLayer(RegistryLayer.STATIC)
+
+    val poolRegistry = worldgen.get(Registries.TEMPLATE_POOL)
+    if(poolRegistry.isEmpty)
+      Unfocused.LOGGER.error("Template Pool registry does not appear to be initialized, aborting template injects.")
+
+
+    val processorLists = worldgen.get(Registries.PROCESSOR_LIST)
+    if(processorLists.isEmpty)
+      Unfocused.LOGGER.error("Processor List registry does not appear to be initialized, aborting template injects.")
+
+    if(poolRegistry.isEmpty || processorLists.isEmpty) return
+
+    injects.forEach { (_, poolInject) ->
+      @Suppress("CAST_NEVER_SUCCEEDS")
+      val pool = poolRegistry.get().value().get(poolInject.pool)!! as StructureTemplatePoolAccessor
+      val templates = pool.templates
+      val rawTemplates = pool.rawTemplates
+
+      poolInject.elements.forEach { element ->
+        val poolElement = SinglePoolElement.legacy(
+            element.element.location,
+            processorLists.get().value().get(ResourceKey.create(
+              Registries.PROCESSOR_LIST,
+              element.element.processors ?: ResourceLocation.parse("minecraft:empty")
+            )).orElseThrow()
+          ).apply(element.element.projection)
+        (0..(element.weight ?: 2)).forEach { _ ->
+          templates.add(poolElement)
+        }
+        pool.rawTemplates = rawTemplates + Pair(poolElement, element.weight!!)
+      }
+    }
   }
 }
--- fabric/src/main/java/com/ssblur/unfocused/mixin/UnfocusedMinecraftMixin.java
+++ fabric/src/main/java/com/ssblur/unfocused/mixin/UnfocusedMinecraftMixin.java
@@ -2,6 +2,7 @@ package com.ssblur.unfocused.mixin;
 
 import com.ssblur.unfocused.event.client.ClientDisconnectEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class UnfocusedMinecraftMixin {
-    @Inject(method = "disconnect()V", at = @At(value = "HEAD"))
-    private void alchimiae$clearLevel(CallbackInfo ci) {
+    @SuppressWarnings("unchecked")
+    @Inject(method = "disconnect", at = @At(value = "HEAD"))
+    private void alchimiae$disconnect(Screen screen, boolean bl, CallbackInfo ci) {
         ClientDisconnectEvent.INSTANCE.callback(Minecraft.getInstance().player);
     }
 }
