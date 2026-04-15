74d9825d3267bb3ac449c012b4e38f5b33874772
Juniper Hallbick <pat.hallbick@outlook.com>
2026-04-15 00:13:01 -0400

    Get 1.21.11 building

--- fabric/src/main/java/com/ssblur/unfocused/mixin/UnfocusedMinecraftMixin.java
+++ fabric/src/main/java/com/ssblur/unfocused/mixin/UnfocusedMinecraftMixin.java
@@ -2,6 +2,7 @@ package com.ssblur.unfocused.mixin;
 
 import com.ssblur.unfocused.event.client.ClientDisconnectEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class UnfocusedMinecraftMixin {
-    @Inject(method = "disconnect()V", at = @At(value = "HEAD"))
-    private void unfocused$clearLevel(CallbackInfo ci) {
+    @Inject(method = "clearClientLevel", at = @At(value = "HEAD"))
+    private void unfocused$clearLevel(Screen screen, CallbackInfo ci) {
         ClientDisconnectEvent.INSTANCE.callback(Minecraft.getInstance().player);
     }
 }
