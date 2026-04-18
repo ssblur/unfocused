36bbb68e8dfebe268a05514e731c51ac58543f11
Juniper Hallbick <pat.hallbick@outlook.com>
2026-04-18 18:02:32 -0400

    Fail easy by default on simple data loaders

--- common/src/main/kotlin/com/ssblur/unfocused/data/DataLoaderRegistry.kt
+++ common/src/main/kotlin/com/ssblur/unfocused/data/DataLoaderRegistry.kt
@@ -28,7 +28,7 @@ object DataLoaderRegistry {
 
   fun <T: Any> ModInitializer.registerSimpleDataLoader(path: String, type: KClass<T>): MutableMap<ResourceLocation, T> {
     val map: MutableMap<ResourceLocation, T> = mutableMapOf()
-    registerDataLoader(path, type as KClass<Any>) { resource, location ->
+    registerDataLoader(path, type as KClass<Any>, failEasy = true) { resource, location ->
       map[location] = resource as T
     }
     return map
