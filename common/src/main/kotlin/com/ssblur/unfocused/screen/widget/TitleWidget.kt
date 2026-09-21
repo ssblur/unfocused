5729602c73bef2ab88e1f8e16f1c5fd9437866b4
Juniper Hallbick <pat.hallbick@outlook.com>
2026-09-20 23:08:52 -0400

    Add Title widget

---
+++ common/src/main/kotlin/com/ssblur/unfocused/screen/widget/TitleWidget.kt
@@ -0,0 +1,28 @@
+package com.ssblur.unfocused.screen.widget
+
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.narration.NarratedElementType
+import net.minecraft.client.gui.narration.NarrationElementOutput
+import net.minecraft.network.chat.Component
+
+class TitleWidget(val text: Component, x: Int, y: Int, w: Int, h: Int, scissor: Boolean = true, var color: UInt = 0x000000u):
+  PositionedWidget(x, y, w, h, scissor) {
+  override fun updateNarration(narrationElementOutput: NarrationElementOutput) {
+    narrationElementOutput.add(NarratedElementType.TITLE, text)
+  }
+
+  override fun draw(
+    guiGraphics: GuiGraphics,
+    mouseX: Int,
+    mouseY: Int,
+    f: Float
+  ) {
+    val scale = 1.8f
+    val pose = guiGraphics.pose()
+    pose.pushPose()
+    pose.scale(scale, scale, scale)
+    guiGraphics.drawWordWrap(Minecraft.getInstance().font, text, 0, 0, w, color.toInt())
+    pose.popPose()
+  }
+}
